c4279ed3bc184806836dd5ee82a5d426ec38f952
Максим Чечульников <ilfirin.nar@gmail.com>
2016-02-27 18:24:33 +0300

    Add some first code to domain

---
+++ src/impl/Solar.Compilier.Domain/Entities/Token.cs
@@ -0,0 +1,9 @@
+﻿using Solar.Infrastructure.Common.Interfaces.DomainLayer;
+
+namespace Solar.Compilier.Domain.Entities
+{
+    internal class Token : IEntity
+    {
+         
+    }
+}
---
+++ src/impl/Solar.Compilier.Domain/ValueObjects/Lexem.cs
@@ -0,0 +1,9 @@
+﻿using Solar.Infrastructure.Common.Interfaces.DomainLayer;
+
+namespace Solar.Compilier.Domain.ValueObjects
+{
+    internal class Lexem : IValueObject
+    {
+         
+    }
+}
