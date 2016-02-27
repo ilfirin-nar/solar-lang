cafcdce996a799c73431aa0ce63295370dddaf0d
Максим Чечульников <ilfirin.nar@gmail.com>
2016-02-28 01:12:44 +0300

    TextFileReader

---
+++ src/Solar.Infrastructure.FileSystem/Services/TextFileReader.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Solar.Infrastructure.FileSystem.Services
+{
+    internal class TextFileReader : ITextFileReader
+    {
+        public string Read(string path)
+        {
+            using (var sr = new StreamReader(path))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
