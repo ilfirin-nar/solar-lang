978baedcc9836db7995d09bc4148d3c100d5449b
Максим Чечульников <ilfirin.nar@gmail.com>
2016-03-02 22:21:13 +0300

    Started implementation test on lexical analyzer

---
+++ src/Solar.Domain.Analysis.Lexical.Tests/LexicalAnalyzerTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using LightInject.xUnit2;
+using Xunit;
+
+namespace Solar.Domain.Analysis.Lexical.Tests
+{
+    public class LexicalAnalyzerTests
+    {
+        [Theory, InjectData]
+        internal void Analyse_ValidOneStatementString_ValidResult(ILexicalAnalyzer analyzer)
+        {
+            const string content = "foo <- bar()";
+            var result = analyzer.Analyse(content);
+            // TODO Finish this!
+            Assert.True(true);
+        }
+    }
+}
